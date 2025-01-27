5b065803fd7ea049e5e58efd0e8f3b09a2027f0c
unknown <rajsriselvan.ca@gmail.com>
2025-01-27 08:17:03 +0000

    JWT error fix deployment

--- wordsmith-tracker-service/src/helper/jwtHelper.ts
+++ wordsmith-tracker-service/src/helper/jwtHelper.ts
@@ -5,6 +5,9 @@ if (!JWT_SECRET) {
   throw new Error("JWT_SECRET is not defined in the environment variables.");
 }
 
-export const generateToken = (payload: object, expiresIn: string = "1h"): string => {
+export const generateToken = (
+  payload: jwt.JwtPayload, 
+  expiresIn: string = "1h" 
+): string => {
   return jwt.sign(payload, JWT_SECRET, { expiresIn });
 };
