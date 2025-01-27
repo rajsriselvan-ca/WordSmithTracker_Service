f307bb20a47de4709de263f9a9635fa919a847d3
unknown <rajsriselvan.ca@gmail.com>
2025-01-27 08:45:59 +0000

    jwt non null

--- wordsmith-tracker-service/src/helper/jwtHelper.ts
+++ wordsmith-tracker-service/src/helper/jwtHelper.ts
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET as string; 
+const JWT_SECRET = process.env.JWT_SECRET!; 
 if (!JWT_SECRET) {
   throw new Error("JWT_SECRET is not defined in the environment variables.");
 }
